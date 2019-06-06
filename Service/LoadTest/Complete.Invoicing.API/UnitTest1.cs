88add36839d519832ffc8e2969b1e99abb5a8db9
tslekwadi <tlekwadi@retrorabbit.co.za>
2019-06-06 11:42:47 +0200

    Initial Service Commit

---
+++ Service/LoadTest/Complete.Invoicing.API/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Complete.Invoicing.API.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
